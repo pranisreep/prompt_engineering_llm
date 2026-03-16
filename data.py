3bfadfa92204209ed33f135aebecb42777b11b66
pranisreep <putlur.p@fidelistech.com>
2026-03-16 11:15:47 +0530

    data.py

---
+++ data.py
@@ -0,0 +1,7 @@
+tickets = [
+    "My payment failed twice but I was charged both times. Please refund.",
+    "App crashes every time I upload a photo on iOS. Happens 100% of the time.",
+    "Dashboard takes 45 seconds to load. It was instant last week.",
+    "Can you add dark mode to the mobile app? Many users want this.",
+    "Someone accessed my account from another country. I didn't authorize it."
+]
